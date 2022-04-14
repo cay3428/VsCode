d999dd0d74d4ef9e1c40a5d5b6fa3a3264c499ab
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-04-14 15:31:39 +0300

    yoola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'northwind';
  user :string='Kaçakçaycı'
- product1={productId:1,productName:'şağak',categoryId:1,unitPrice:777}
- product2={productId:2,productName:'patapata',categoryId:1,unitPrice:777}
- product3={productId:3,productName:'vatantatatan',categoryId:1,unitPrice:777}
- product4={productId:4,productName:'şaaaak',categoryId:1,unitPrice:777}
- product5={productId:5,productName:'adınıs asizm<i',categoryId:1,unitPrice:777}
- product6={productId:6,productName:'o ne lan',categoryId:1,unitPrice:777}
-
-products=[this.product1,this.product2,this.product3,this.product4,this.product5,this.product6];
 
 
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+
 
 @Component({
   selector: 'app-product',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  
+ 
+ products:Product[]=[];
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+/* product1={productId:1,productName:'şağak',categoryId:1,unitPrice:777,unitsInStock:5}
+  product2={productId:2,productName:'patapata',categoryId:1,unitPrice:777,unitsInStock:31}
+  product3={productId:3,productName:'vatantatatan',categoryId:1,unitPrice:777,unitsInStock:81}
+  product4={productId:4,productName:'şaaaak',categoryId:1,unitPrice:777,unitsInStock:999}
+  product5={productId:5,productName:'adınıs asizm<i',categoryId:1,unitPrice:777,unitsInStock:69}
+  product6={productId:6,productName:'o ne lan',categoryId:1,unitPrice:777,unitsInStock:100} */
+/*   this.product1,this.product2,this.product3,this.product4,this.product5,this.product6] */

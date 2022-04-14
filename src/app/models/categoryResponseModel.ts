509c4498754a152c171370d966ee9a94dc201315
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-04-14 15:53:07 +0300

    yolla

---
+++ src/app/models/categoryResponseModel.ts
@@ -0,0 +1,9 @@
+import { Category } from "./category";
+import { ResponseModel } from "./responseModel";
+
+export interface CategoryResponseModel extends ResponseModel{
+
+    data:Category[]
+
+    
+}

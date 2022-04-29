a2658b99a5a7c4f9983db0b8d5f0dba7a010c26c
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-04-29 15:58:16 +0300

    kartiemei

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { listenerCount } from 'process';
+import { CartItem } from '../models/cartItem';
+import { CartItems } from '../models/cartItems';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+  addToCart(product:Product){
+    let item = CartItems.find(c=>c.product.productId===product.productId);
+if(item){
+  item.quantity+=1;
+}
+else{
+  let cartItem=new CartItem();
+  CartItems.push()
+  cartItem.product=product;
+  cartItem.quantity=1;
+}
+
+  }
+  list():CartItem[]{
+    return CartItems;
+  }
+  
+}
